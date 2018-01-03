11c27f4b4258674d3758c20fd5e286334e077f7a
HKW <HKW@yahoo.com>
2018-01-03 16:17:32 +0800

    Initial commit

---
+++ app/src/main/java/com/n9s/flyjet/hk2018010302/MainActivity.java
@@ -0,0 +1,30 @@
+package com.n9s.flyjet.hk2018010302;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity
+{
+    WebView wv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        wv = (WebView) findViewById(R.id.webView);
+
+        //wv.setWebViewClient(new WebViewClient());
+        wv.setWebChromeClient(new WebChromeClient());
+
+        wv.getSettings().setJavaScriptEnabled(true);  //開啟Java Script
+
+        //wv.loadUrl("http://www.youtube.com");
+        //wv.loadUrl("http://www.hinet.net");
+        wv.loadData("<iframe width=\"480\" height=\"300\" src=\"https://www.youtube.com/embed/JwjBbWQs71k\" frameborder=\"0\" gesture=\"media\" allow=\"encrypted-media\" allowfullscreen></iframe>", "text/html; charset=UTF-8", null);
+    }
+}
